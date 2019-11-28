5797b5aa72e8af4af7a7885bf8045fc66dce423b
untakenyjp <1980344834@qq.com>
2019-11-28 22:37:06 +0800

    add contributer

--- BankManage/BankManage/money/Withdraw.xaml.cs
+++ BankManage/BankManage/money/Withdraw.xaml.cs
@@ -28,13 +28,19 @@ namespace BankManage.money
         //取款
         private void btnOk_Click(object sender, RoutedEventArgs e)
         {
+            //根据对应id创建业务记录
             Custom custom = DataOperation.GetCustom(this.txtAccount.Text);
+
+            //验证密码
             if (custom.AccountInfo.accountPass != this.txtPassword.Password)
             {
                 MessageBox.Show("密码不正确");
                 return;
             }
+            //执行取款操作
             custom.Withdraw(double.Parse(this.txtmount.Text));
+
+            //导航到历史操作记录页面
             OperateRecord page = new OperateRecord();
             NavigationService ns = NavigationService.GetNavigationService(this);
             ns.Navigate(page);
@@ -42,6 +48,7 @@ namespace BankManage.money
         //取消取款
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
+            //导航到历史操作记录页面
             OperateRecord page = new OperateRecord();
             NavigationService ns = NavigationService.GetNavigationService(this);
             ns.Navigate(page);
